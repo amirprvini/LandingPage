ef63af9373452ccdd418fd9342fbe860f9bf0ea4
amirparvini <amirparvini2000@gmail.com>
2024-08-22 19:02:57 +0330

    mobile navbar componenet created

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { navItems } from '../../constants';
+import {Menu , X} from 'lucide-react'
+import MobileNavBar from '../MobileNavBar';
 
 interface navBarProps {}
 
 const NavBar: React.FC<navBarProps> = () : JSX.Element => {
-  return <div className="navBar bg-black text-white flex justify-between items-center border-b-2 border-gray-300 h-14 px-2 font-Poppins">
+
+    const [isClicked,setIsClicked] = useState<boolean>(false) ;
+
+  return <div className="navBar flex flex-col">
         
-        <div className="logoWrapper flex space-x-4">
+        <div className="navBarContainer w-full bg-black text-white flex justify-between px-6 lg:justify-around lg:px-0 items-center border-b border-neutrul-700/80 h-16 sticky top-0 backdrop-blur-lg">
+
+            <div className="logoWrapper flex justify-center items-center space-x-2">
             <div className="logoIconWrapper">
-                <img src="./my-app/images/logo.png" alt="headerLogo" />
+                <img src="./images/logo.png" alt="headerLogo" width={'30px'} height={'30px'} />
             </div>
-            <h3 className="logoTitle"> Landing Page </h3>
+            <h3 className="logoTitle text-lg"> Landing Page </h3>
         </div>
 
-        <div className="navBarItemsWrapper">
+        <div className="navBarItemsWrapper hidden lg:block">
             <ul className="iltemsList flex gap-6">
                 {navItems.map((navItem,index)=>{
                     return <li key={index}>
@@ -23,11 +30,23 @@ const NavBar: React.FC<navBarProps> = () : JSX.Element => {
             </ul>
         </div>
 
-        <div className="navBarButtonsWrapper flex gap-2">
+        <div className="navBarButtonsWrapper space-x-10 hidden lg:flex">
+
                 <a href={'#'} className="border px-2 py-1 text-white rounded-md font-semibold"> Sing in </a>
-                
                 <a href={'#'} className="border px-2 py-1 text-white rounded-md font-semibold  bg-gradient-to-r from-orange-500 to-orange-800"> Create an account </a>
+        
         </div>
+
+        <div className="navBarMenuWrapper flex lg:hidden">
+                <button className="navBarMenu" onClick={()=>{setIsClicked(!isClicked)}}>
+                    {isClicked ? <X /> : <Menu />}
+                </button>
+        </div>
+
+    </div>
+    
+        { isClicked ? <MobileNavBar /> : null }
+
   </div>
 }
 
